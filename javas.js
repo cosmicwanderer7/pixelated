9eeda629799b0323e6a7a673d8e880e18272bab5
cosmicwanderer7 <yewaleprithvi2003@gmail.com>
2023-07-13 16:47:14 +0530

    shuffle

---
+++ javas.js
@@ -0,0 +1,55 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ248927897989659";
+
+let interval = null;
+
+document.getElementById("sub").onmouseover = (event) => {
+  let iteration = 0;
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return event.target.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= event.target.dataset.value.length) {
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 3;
+  }, 49);
+
+  
+};
+
+document.getElementById("title").onmouseover = (event) => {
+  let iteration = 0;
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return event.target.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= event.target.dataset.value.length) {
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 5;
+  }, 49);
+};
