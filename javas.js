f2fd58f1dd1d42a7504053cceb5b1e95f5017e14
cosmicwanderer7 <yewaleprithvi2003@gmail.com>
2023-07-20 13:48:45 +0530

    animation

--- javas.js
+++ javas.js
@@ -1,55 +1,75 @@
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ248927897989659";
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let intervalTitle = null;
+let intervalSub = null;
 
-let interval = null;
-
-document.getElementById("sub").onmouseover = (event) => {
+// Function to run on mouseover and every second for "sub" element
+function updateTextSub(element) {
   let iteration = 0;
 
-  clearInterval(interval);
+  clearInterval(intervalSub);
 
-  interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
+  intervalSub = setInterval(() => {
+    element.innerText = element.innerText
       .split("")
       .map((letter, index) => {
         if (index < iteration) {
-          return event.target.dataset.value[index];
+          return element.dataset.value[index];
         }
 
         return letters[Math.floor(Math.random() * 26)];
       })
       .join("");
 
-    if (iteration >= event.target.dataset.value.length) {
-      clearInterval(interval);
+    if (iteration >= element.dataset.value.length) {
+      clearInterval(intervalSub);
+      // Once the "sub" interval is complete, start the "title" interval again
+      updateTextTitle(document.getElementById("title"));
     }
 
-    iteration += 1 / 3;
+    iteration += 1;
   }, 49);
+}
 
-  
-};
-
-document.getElementById("title").onmouseover = (event) => {
+// Function to run every second for "title" element
+function updateTextTitle(element) {
   let iteration = 0;
 
-  clearInterval(interval);
+  clearInterval(intervalTitle);
 
-  interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
+  intervalTitle = setInterval(() => {
+    element.innerText = element.innerText
       .split("")
       .map((letter, index) => {
         if (index < iteration) {
-          return event.target.dataset.value[index];
+          return element.dataset.value[index];
         }
 
         return letters[Math.floor(Math.random() * 26)];
       })
       .join("");
 
-    if (iteration >= event.target.dataset.value.length) {
-      clearInterval(interval);
+    if (iteration >= element.dataset.value.length) {
+      clearInterval(intervalTitle);
+      // Once the "title" interval is complete, start the "sub" interval
+      document.getElementById("sub").addEventListener("mouseover", () => {
+        updateTextSub(document.getElementById("sub"));
+      });
     }
 
-    iteration += 1 / 5;
+    iteration += 1;
   }, 49);
-};
+}
+
+// Start the initial interval for "title" element
+updateTextTitle(document.getElementById("title"));
+
+// Function to start the intervals again every 4 seconds
+function startIntervals() {
+  updateTextTitle(document.getElementById("title"));
+  setTimeout(() => {
+    updateTextSub(document.getElementById("sub"));
+  }, 4000);
+}
+
+// Start the intervals every 4 seconds
+setInterval(startIntervals, 4000);
